a8a7cc6e87c09455522a757af8fd3a1eae621683
Alex <alex-ii@mail.ru>
2013-05-30 14:17:49 +0600

    future: tabs

--- templates/gantry/html/com_uvelir/products/default.php
+++ templates/gantry/html/com_uvelir/products/default.php
@@ -10,12 +10,17 @@
 defined('_JEXEC') or die;
 //var_dump($this->item);
 ?>
-<style type="text/css">
-    .active{
-        color:red;
-    }
-</style>
-<a class="<?=$this->products_group=='1'?'active':''?>"href="<?php echo JRoute::_('novinki')?>" class="trigger"><?=  JText::_('COM_UVELIR_PRODUCT_NEW')?></a>
-<a class="<?=$this->products_group=='2'?'active':''?>" href="<?php echo JRoute::_('spetsialnye-predlozheniya')?>" class="trigger"><?=  JText::_('COM_UVELIR_PRODUCT_SPETS')?></a>
-<a class="<?=$this->products_group?'':'active'?>" href="<?php echo JRoute::_('katalog-izdelij')?>" class="trigger"><?=  JText::_('COM_UVELIR_PRODUCT_ALL')?></a>
+<ul class="tabs">
+	<li class="first">
+		<a class="<?=$this->products_group=='1'?'active':''?>" href="<?php echo JRoute::_('novinki')?>" class="trigger"><?=  JText::_('COM_UVELIR_PRODUCT_NEW')?></a>
+	</li>
+	<li class="separator"></li>
+	<li>
+		<a class="<?=$this->products_group=='2'?'active':''?>" href="<?php echo JRoute::_('spetsialnye-predlozheniya')?>" class="trigger"><?=  JText::_('COM_UVELIR_PRODUCT_SPETS')?></a>
+	</li>
+	<li class="separator"></li>
+	<li class="last">
+		<a class="<?=$this->products_group?'':'active'?>" href="<?php echo JRoute::_('katalog-izdelij')?>" class="trigger"><?=  JText::_('COM_UVELIR_PRODUCT_ALL')?></a>
+	</li>
+</ul>
 <?php echo $this->loadTemplate('products');?>
